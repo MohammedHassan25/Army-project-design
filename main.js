22bed46f942cd019ddc75b2ba3396b698e3f5b1d
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2023-07-04 01:45:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var x = setInterval(function() {
+  let dateArmy = new Date("Nov 25, 2023");
+  let dateNow = new Date();
+
+  let date = dateArmy - dateNow;
+
+  var days = Math.floor(date / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((date % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((date % (1000 * 60)) / 1000);
+
+  document.querySelector("h1.time").cssText = "direction: rtl;"
+
+  document.querySelector("h1.time").innerHTML = `${days} يوم : ${hours} ساعة : ${minutes} دقيقة : ${seconds} ثانية`
+  // If the count down is finished, write some text
+  if (date < 0) {
+    clearInterval(x);
+    document.querySelector("h1.time").innerHTML = "انتهاء الوقت ... خلاص خلصنا جيش ✨🥳🥳";
+  }
+}, 1000);
